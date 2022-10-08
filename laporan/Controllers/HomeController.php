c2bb51ecdf37bed2498d3e5f8a2c3fbe5dcdf76b
ahmadfadillllah <ahmadfadillah502@gmail.com>
2022-10-08 17:02:45 +0800

    Update customer

--- laporan/Controllers/HomeController.php
+++ laporan/Controllers/HomeController.php
@@ -56,7 +56,7 @@ class HomeController extends Controller
     public function login_register_post_register(Request $request)
     {
         $credentials = $request->validate([
-            'email' => ['required', 'email', 'unique:email'],
+            'email' => ['required', 'email', 'unique:users,email'],
             'password' => ['required', 'confirmed', 'min:8'],
         ]);
 
